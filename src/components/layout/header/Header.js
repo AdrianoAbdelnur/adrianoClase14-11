82cde72ba4f040fd7249571978aae46061105dbe
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2022-11-14 21:28:46 -0300

    practica clase 14-11

---
+++ src/components/layout/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header_container'>
+        <button>LOGIN</button>
+    </div>
+  )
+}
+
+export default Header
